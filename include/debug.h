8bf954ea1a26e90128bbc27243aeb7be087ce3fc
22050908-Meng Pei <2398696309@qq.com>
2023-06-17 23:20:14 +0800

    backtrace

--- include/debug.h
+++ include/debug.h
@@ -4,11 +4,31 @@
 #include "lib/stdio.h"
 #include "macro.h"
 #include "riscv64.h"
+#include "common.h"
+#include "memory/pm.h"
+
+static inline void backtrace(void) {
+  printf("backtrace:\n");
+
+  uint64_t fp = READ_GRR(fp);
+
+  while (true) {
+    printf("0x%p\n", *((uint64_t *)(fp - 8)));
+    uint64_t prefp = *((uint64_t *)(fp - 16));
+
+    if (PAGE_START(fp) != PAGE_START(prefp)) {
+      break;
+    }
+
+    fp = prefp;
+  }
+}
 
 static inline void panic() {
   printf(ANSI_FMT("sstatus: 0x%p\n", ANSI_FG_RED), READ_CSR(s, status));
   printf(ANSI_FMT("sepc: 0x%p\n", ANSI_FG_RED), READ_CSR(s, epc));
   printf(ANSI_FMT("scause: 0x%p\n", ANSI_FG_RED), READ_CSR(s, cause));
+  backtrace();
 
   printf(ANSI_FMT("panic", ANSI_FG_GREEN ANSI_BG_RED) "\n");
   for (;;);
