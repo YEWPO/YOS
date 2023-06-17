901022ee38d4bdb7df9710e1f91c79899152dbba
22050908-Meng Pei <2398696309@qq.com>
2023-06-17 21:11:30 +0800

    缓冲区测试

--- kernel/test.c
+++ kernel/test.c
@@ -6,4 +6,22 @@
 
 void test() {
   Log("test");
+
+  struct buffer_block *buffer;
+
+  buffer = buffer_acquire(BLOCK_DEVICE, 0);
+
+  for (int i = 0; i < 512; ++i) {
+    printf("id %d, data %x\n", i, buffer->data[i]);
+  }
+
+  buffer->data[0] = 'm';
+  buffer->data[1] = 'p';
+  buffer->dirty = true;
+
+  buffer_release(buffer);
+
+  for (int i = 0; i < 512; ++i) {
+    printf("id %d, data %x\n", i, buffer->data[i]);
+  }
 }
